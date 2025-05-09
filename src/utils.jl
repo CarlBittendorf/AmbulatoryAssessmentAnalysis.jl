d320aaeac98a293f8fce482d10c2328971aedfd5
Carl Bittendorf <carl.bittendorf@gmail.com>
2025-05-09 16:22:51 +0200

    Add utility functions

--- src/utils.jl
+++ src/utils.jl
@@ -7,8 +7,9 @@
 # EXPORTS
 ####################################################################################################
 
-export fill_down, enumerate_days, chunk, duration_to_next, duration_to_previous,
-       groupby_period, insert_period_starts
+export fill_down, enumerate_days, enumerate_clusters, chunk, duration_to_next,
+       duration_to_previous, groupby_period, insert_period_starts, fill_periods,
+       count_unique, count_changes, frequencies_of_occurrence
 
 ####################################################################################################
 # IMPLEMENTATIONS
@@ -68,6 +69,43 @@ julia> enumerate_days(Date.(["2025-02-01", "2025-02-02", "2025-01-01", "2025-01-
 ```
 """
 enumerate_days(x) = Dates.value.(Date.(x) .- Date(minimum(x))) .+ 1
+
+"""
+    enumerate_clusters(x) -> Vector{Int}
+
+Enumerate clustered points that are consecutive in time.
+
+# Examples
+
+```jldoctest
+julia> enumerate_clusters([3, 3, 8, 5, 5, 5, 3, 8, 8])
+9-element Vector{Int64}:
+ 1
+ 1
+ 2
+ 3
+ 3
+ 3
+ 4
+ 5
+ 5
+```
+"""
+function enumerate_clusters(x)
+    labels = ones(Int, length(x))
+
+    for i in eachindex(x)
+        i == 1 && continue
+
+        if x[i] == x[i - 1]
+            labels[i] = labels[i - 1]
+        else
+            labels[i] = labels[i - 1] + 1
+        end
+    end
+
+    return labels
+end
 
 """
     chunk(N, size) -> Vector{UnitRange{Int64}}
@@ -230,6 +268,122 @@ function insert_period_starts(
     @chain df begin
         vcat(df_extra; cols = :union)
         sort([groupcols..., timecol])
+    end
+end
+
+"""
+    fill_periods(df, period, step; timecol = :DateTime, groupcols = []) -> DataFrame
+
+Fill each `period` in the dataframe with rows spaced by `steps`, but do not add any entry that is before the first
+or after the last timestamp of the respective participant.
+
+The `timecol` column contains the timestamps. The groupcols argument should be used if,
+for example, there are multiple participants in the dataframe.
+
+# Examples
+
+```jldoctest
+julia> using DataFrames, Dates
+
+julia> df = DataFrame(:DateTime => Date("2025-01-01") .+ Time.(["10:05:00", "12:45:00"]))
+2×1 DataFrame
+ Row │ DateTime            
+     │ DateTime            
+─────┼─────────────────────
+   1 │ 2025-01-01T10:05:00
+   2 │ 2025-01-01T12:45:00
+
+julia> fill_periods(df, Hour(1), Minute(1))
+101×1 DataFrame
+ Row │ DateTime            
+     │ DateTime            
+─────┼─────────────────────
+   1 │ 2025-01-01T10:05:00
+   2 │ 2025-01-01T10:06:00
+   3 │ 2025-01-01T10:07:00
+  ⋮  │          ⋮
+  99 │ 2025-01-01T12:43:00
+ 100 │ 2025-01-01T12:44:00
+ 101 │ 2025-01-01T12:45:00
+            95 rows omitted
+```
+"""
+function fill_periods(
+        df::DataFrame, period::Period, step; timecol = :DateTime, groupcols = [])
+    df_filled = @chain df begin
+        groupby(groupcols)
+        transform(
+            timecol => minimum => :Start,
+            timecol => maximum => :End
+        )
+        groupby_period(period; groupcols)
+        combine([timecol, :Start, :End]
+        => ((x, s, e) -> range(max(first(s), first(x)), min(first(e), first(x) + period - step); step)) => :DateTimeFilled)
+        select(Not(timecol))
+        rename(:DateTimeFilled => timecol)
+    end
+
+    @chain df begin
+        outerjoin(df_filled; on = [groupcols..., timecol])
+        sort([groupcols..., timecol])
+    end
+end
+
+"""
+    count_unique(x) -> Int
+
+Count the unique number of elements in `x`.
+
+# Examples
+
+```jldoctest
+julia> count_unique([3, 3, 8, 5, 5, 5, 3, 8, 8])
+3
+
+julia> count_unique(1:10)
+10
+```
+"""
+count_unique(x) = length(unique(x))
+
+"""
+    count_changes(x) -> Int
+
+Count the number of consecutive elements that are different from each other.
+
+# Examples
+
+```jldoctest
+julia> count_changes([3, 3, 8, 5, 5, 5, 3, 8, 8])
+4
+
+julia> count_changes(collect(1:10))
+9
+```
+"""
+count_changes(x) = count(x .!= ShiftedArrays.lag(x; default = first(x)))
+
+"""
+    frequencies_of_occurrence(x) -> Vector{Int}
+
+Return the frequencies of occurrence for each unique element in `x`.
+
+# Examples
+
+```jldoctest
+julia> frequencies_of_occurrence([3, 3, 8, 5, 5, 5, 3, 8, 8])
+3-element Vector{Int64}:
+ 3
+ 3
+ 3
+```
+"""
+function frequencies_of_occurrence(x)
+    # this is equivalent to but ~5x faster than `[count(isequal(e), x) for e in unique(x)]`
+    @chain DataFrame(X = x) begin
+        groupby(:X)
+        combine(nrow)
+        getproperty(:nrow)
     end
 end
 
